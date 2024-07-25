c814ab5986a7f9d3d802f4f298eb9007535079bb
liza <122369834+lizasizas@users.noreply.github.com>
2024-07-25 13:31:08 +0700

    update slinding window.sql

--- 03 Advanced SQL/slinding window.sql
+++ 03 Advanced SQL/slinding window.sql
@@ -1,14 +1,12 @@
--- SLIDING WINDOW
-
 SELECT 
 	m.id, 
-  t.team_long_name,
+	t.team_long_name,
 	CASE WHEN m.home_goal > m.away_goal THEN 'MU Win'
 		WHEN m.home_goal < m.away_goal THEN 'MU Loss'
-    ELSE 'Tie' END AS outcome
+		ELSE 'Tie' END AS outcome
 FROM match AS m
 LEFT JOIN team AS t 
 ON m.hometeam_id = t.team_api_id
-WHERE 
+WHERE
 	season = '2014/2015'
 	AND t.team_long_name = 'Manchester United';
