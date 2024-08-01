b0658f45eb313fbb814c868eb1a6263f668c00d9
liza <122369834+lizasizas@users.noreply.github.com>
2024-08-01 14:01:13 +0700

    Update submission.sql

--- 04 Practice/01 DataLemur/IBM db2 Product Analytics/submission.sql
+++ 04 Practice/01 DataLemur/IBM db2 Product Analytics/submission.sql
@@ -2,7 +2,7 @@ WITH CTE AS (
   SELECT
     e.employee_id,
     COALESCE(COUNT(DISTINCT query_id), 0) AS unique_queries
-  FROM employees AS 
+  FROM employees AS e
   LEFT JOIN queries AS q ON q.employee_id=e.employee_id
     AND q.query_starttime BETWEEN '2023-07-01' AND '2023-10-01'
   GROUP BY e.employee_id
