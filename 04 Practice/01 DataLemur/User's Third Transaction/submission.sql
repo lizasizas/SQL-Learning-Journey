78523793cba25c3cba860d2d0617d5055b1d4abe
liza <122369834+lizasizas@users.noreply.github.com>
2024-08-14 19:23:13 +0700

    Update submission.sql

--- 04 Practice/01 DataLemur/User's Third Transaction/submission.sql
+++ 04 Practice/01 DataLemur/User's Third Transaction/submission.sql
@@ -1,13 +1,13 @@
 WITH CTE AS (
-SELECT
-  user_id,
-  spend,
-  transaction_date,
-  ROW_NUMBER() OVER (
-    PARTITION BY user_id ORDER BY transaction_date) AS row_num
-FROM transactions
+  SELECT
+    user_id,
+    spend,
+    transaction_date,
+    ROW_NUMBER() OVER (
+      PARTITION BY user_id ORDER BY transaction_date) AS row_num
+  FROM transactions
 )
-
+  
 SELECT
   user_id,
   spend,
