82158f806d52a2d13f7a80dd258aeb615f2cc580
liza <122369834+lizasizas@users.noreply.github.com>
2025-07-14 20:55:07 +0700

    Update submission.sql

--- 04 Practice/03 HackerRank/39 The PADS/submission.sql
+++ 04 Practice/03 HackerRank/39 The PADS/submission.sql
@@ -1,6 +1,24 @@
 SELECT
-    co.continent, FLOOR(AVG(ci.population))
-FROM city ci
-JOIN country co
-    ON ci.countrycode = co.code
-GROUP BY co.continent;
+    CASE WHEN occupation = 'Doctor' THEN CONCAT(name, '(D)')
+        WHEN occupation = 'Actor' THEN CONCAT(name, '(A)')
+        WHEN occupation = 'Singer' THEN CONCAT(name, '(S)')
+        WHEN occupation = 'Professor' THEN CONCAT(name, '(P)') END
+FROM occupations
+ORDER BY name;
+SELECT CONCAT('There are a total of ', COUNT(name), ' ', LOWER(occupation), 's.')
+FROM occupations
+GROUP BY occupation
+ORDER BY COUNT(name), occupation;
+
+-- oracle
+SELECT
+    CASE WHEN occupation = 'Doctor' THEN name || '(D)'
+        WHEN occupation = 'Actor' THEN name || '(A)'
+        WHEN occupation = 'Singer' THEN name || '(S)'
+        WHEN occupation = 'Professor' THEN name || '(P)' END
+FROM occupations
+ORDER BY name;
+SELECT 'There are a total of '  || COUNT(name) || ' ' || LOWER(occupation) || 's.'
+FROM occupations
+GROUP BY occupation
+ORDER BY COUNT(name), occupation;
